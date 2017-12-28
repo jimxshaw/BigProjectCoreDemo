346a73768bbcf62808aeed47655875c2ce500821
Jim Shaw <jimxshaw@gmail.com>
2017-12-27 20:50:04 -0600

    Add some jquery to manipulate dom.

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,1 +1,10 @@
-﻿
+﻿$(document).ready(function () {
+  $("#theForm").hide();
+
+  var $loginToggle = $("#loginToggle");
+  var $popupForm = $(".popupForm");
+
+  $loginToggle.on("click", function() {
+    $popupForm.fadeToggle(1000);
+  });
+});
