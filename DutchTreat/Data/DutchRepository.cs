c97816f59fd806ce8aa090b59cc25d22a602ba44
Jim Shaw <jimxshaw@gmail.com>
2018-06-24 18:47:21 -0500

    Add logging options to config json file. Wrap repo methods in try-catch blocks.

--- DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/Data/DutchRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,28 +11,55 @@ namespace DutchTreat.Data
   public class DutchRepository : IDutchRepository
   {
     private readonly DutchContext _context;
+    private readonly ILogger<DutchRepository> _logger;
 
-
-    public DutchRepository(DutchContext context)
+    public DutchRepository(DutchContext context, ILogger<DutchRepository> logger)
     {
       _context = context;
+      _logger = logger;
     }
 
 
     public IEnumerable<Product> GetProducts()
     {
-      return _context.Products
-                     .OrderBy(p => p.Title)
-                     .ToList();
+      try
+      {
+        _logger.LogInformation("Get products was called...");
+
+        return _context.Products
+                       .OrderBy(p => p.Title)
+                       .ToList();
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError($"Failed to get products: {ex}");
+
+        return null;
+      }
+
+
     }
 
 
     public IEnumerable<Product> GetProductsByCategory(string category)
     {
-      return _context.Products
-                     .Where(p => p.Category == category)
-                     .OrderBy(p => p.Title)
-                     .ToList();
+      try
+      {
+        _logger.LogInformation("Get products by category was called...");
+
+        return _context.Products
+                             .Where(p => p.Category == category)
+                             .OrderBy(p => p.Title)
+                             .ToList();
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError($"Failed to get products by category: {ex}");
+
+        return null;
+      }
+
+
     }
 
 
