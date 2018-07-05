4f6609d07912bbc16ac61f00b413f0c423b1b100
Jim Shaw <jimxshaw@gmail.com>
2018-07-05 16:08:37 -0500

    Add StoreUser class. Make dutch context derive from identity context that use the new StoreUser class.

--- DutchTreat/Data/Entities/Order.cs
+++ DutchTreat/Data/Entities/Order.cs
@@ -9,8 +9,15 @@ namespace DutchTreat.Data.Entities
   public class Order
   {
     public int Id { get; set; }
+
     public DateTime OrderDate { get; set; }
+
     public string OrderNumber { get; set; }
+
     public ICollection<OrderItem> Items { get; set; }
+
+    public StoreUser User { get; set; }
+
+
   }
 }
