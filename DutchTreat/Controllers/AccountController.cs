577a85b8f9ae7f49ef332bf766531ebf5013091d
Jim Shaw <jimxshaw@gmail.com>
2018-07-14 16:43:46 -0500

    Finish implementation of CreateToken action. Add configuration to Jwt options.

--- DutchTreat/Controllers/AccountController.cs
+++ DutchTreat/Controllers/AccountController.cs
@@ -3,10 +3,14 @@ using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,14 +21,17 @@ namespace DutchTreat.Controllers
     private readonly ILogger<AccountController> _logger;
     private readonly SignInManager<StoreUser> _signInManager;
     private readonly UserManager<StoreUser> _userManager;
+    private readonly IConfiguration _config;
 
     public AccountController(ILogger<AccountController> logger,
       SignInManager<StoreUser> signInManager,
-      UserManager<StoreUser> userManager)
+      UserManager<StoreUser> userManager,
+      IConfiguration config)
     {
       _logger = logger;
       _signInManager = signInManager;
       _userManager = userManager;
+      _config = config;
     }
 
 
@@ -93,7 +100,32 @@ namespace DutchTreat.Controllers
           if (result.Succeeded)
           {
             // Create the token.
+            var claims = new[]
+            {
+              new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+              new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+              new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
+            };
 
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+              _config["Tokens:Issuer"],
+              _config["Tokens:Audience"],
+              claims,
+              expires: DateTime.UtcNow.AddMinutes(30),
+              signingCredentials: credentials
+            );
+
+            var results = new
+            {
+              token = new JwtSecurityTokenHandler().WriteToken(token),
+              expiration = token.ValidTo
+            };
+
+            return Created("", results);
           }
         }
 
