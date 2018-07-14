5c87e158e830eed94c1ab15c6191737998b556b9
Jim Shaw <jimxshaw@gmail.com>
2018-07-14 17:04:53 -0500

    Refactor create an order to be async.

--- DutchTreat/Controllers/OrdersController.cs
+++ DutchTreat/Controllers/OrdersController.cs
@@ -4,6 +4,7 @@ using DutchTreat.Data.Entities;
 using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -21,14 +22,17 @@ namespace DutchTreat.Controllers
     private readonly IDutchRepository _repository;
     private readonly ILogger<OrdersController> _logger;
     private readonly IMapper _mapper;
+    private readonly UserManager<StoreUser> _userManager;
 
     public OrdersController(IDutchRepository repository,
                             ILogger<OrdersController> logger,
-                            IMapper mapper)
+                            IMapper mapper,
+                            UserManager<StoreUser> userManager)
     {
       _repository = repository;
       _logger = logger;
       _mapper = mapper;
+      _userManager = userManager;
     }
 
 
@@ -79,7 +83,7 @@ namespace DutchTreat.Controllers
 
 
     [HttpPost]
-    public IActionResult Post([FromBody]OrderViewModel model)
+    public async Task<IActionResult> Post([FromBody]OrderViewModel model)
     {
       try
       {
@@ -91,6 +95,9 @@ namespace DutchTreat.Controllers
           {
             newOrder.OrderDate = DateTime.Now;
           }
+
+          var currentUser = await _userManager.FindByNameAsync(User.Identity.Name);
+          newOrder.User = currentUser;
 
           _repository.AddEntity(newOrder);
 
